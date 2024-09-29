68b6ef144558f081e905de8bd89d5989035f81df
Kelvin Gikonyo <gikonyosenior@gmail.com>
2024-09-29 17:16:54 +0300

    complete functions lab

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solution in this file!
+let ScuberHq = 42;
+function distanceFromHqInBlocks(pickupLocation){
+    if (pickupLocation > 42){
+        return pickupLocation - 42;
+    } else {
+        return 42 - pickupLocation;
+    }
+}; 
+
+function distanceFromHqInFeet(pickupLocation){
+    return distanceFromHqInBlocks(pickupLocation) * 264;
+};
+
+function distanceTravelledInFeet(start, destination){
+    if (destination > start){
+        return (destination - start) * 264;
+    } else {
+        return (start - destination) * 264;
+    }
+};
+
+function calculatesFarePrice(start, destination){
+    const distance = distanceTravelledInFeet(start, destination);
+    if (distance <= 400){
+        return 0;
+    } else if (distance > 400 && distance <= 2000){
+        return (distance- 400) * 0.02;
+    } else if (distance > 2000 && distance < 2500){
+        return 25;
+    } else {
+        return 'cannot travel that far';
+    }
+    
+    }
